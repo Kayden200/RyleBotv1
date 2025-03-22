849923227189168e0b2470d94bd99478f24a8f69
Kayden200 <159207295+Kayden200@users.noreply.github.com>
2025-03-22 19:32:27 +0800

    Update models.js

--- models.js
+++ models.js
@@ -9,3 +9,30 @@ const ShareLogSchema = new mongoose.Schema({
 const ShareLog = mongoose.model("ShareLog", ShareLogSchema);
 
 module.exports = { ShareLog };
+
+
+
+const mongoose = require("mongoose");
+
+const AppStateSchema = new mongoose.Schema({
+  email: String,
+  appState: Array,
+  timestamp: { type: Date, default: Date.now }
+});
+
+const AppState = mongoose.model("AppState", AppStateSchema);
+
+module.exports = { AppState };
+
+
+const mongoose = require("mongoose");
+
+const UserTokenSchema = new mongoose.Schema({
+  email: String,
+  token: String,
+  timestamp: { type: Date, default: Date.now }
+});
+
+const UserToken = mongoose.model("UserToken", UserTokenSchema);
+
+module.exports = { UserToken };
